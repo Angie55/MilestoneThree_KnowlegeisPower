a91b983970c7bf3d2a493f4bec7bbf6ef5864da6
Ubuntu <ubuntu@ip-172-31-90-252.ec2.internal>
2019-12-21 13:31:09 +0000

    connecting to Mongodb with an environment variable  and testing it with a function

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 import os
 from flask import Flask
+import pymongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World'
+#using the OS library to set a constant by using the getenv() method to read in the environment variable
+#also using the Python constants to access the database and collection
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "knowledge_is_power"
+COLLECTION_NAME = "fundraisers"
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)  
+#Mongo connect function to print message is connection is successful or there is an error
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+        
+#calling the connect function to print everything in the database when called in terminal      
+        
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)   
+        
+
+
